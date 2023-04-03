53f4fdd07e60b108f1f4e19a33c648e465ff2ee0
gitShelly <avishigupta0719@gmail.com>
2023-04-03 19:04:27 +0530

    initial commit

--- traveling_sales.py
+++ traveling_sales.py
@@ -2,11 +2,15 @@ from sys import maxsize
 from itertools import permutations
 V = 4
 
+graph = [[0, 10, 15, 20], [10, 0, 35, 25],
+		[15, 35, 0, 30], [20, 25, 30, 0]]
+
+
 def travellingSalesmanProblem(graph, s):
 	vertex = []
 	for i in range(V):
 		if i != s:
-			vertex.append(i)
+			vertex.append(i)			
 	min_path = maxsize
 	next_permutation=permutations(vertex)
 	for i in next_permutation:
@@ -17,11 +21,9 @@ def travellingSalesmanProblem(graph, s):
 			k = j
 		current_pathweight += graph[k][s]
 		min_path = min(min_path, current_pathweight)
-		
+    
+    
 	return min_path
 
-if __name__ == "__main__":
-	graph = [[0, 10, 15, 20], [10, 0, 35, 25],
-			[15, 35, 0, 30], [20, 25, 30, 0]]
-	s = -1
-	print(travellingSalesmanProblem(graph, s))
+s = -1
+print(travellingSalesmanProblem(graph, s))
